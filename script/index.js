2aaf9e902ab486d0602ea121adcab2fd544d5bc8
alokkumar-dev <alokshakya2601@gmail.com>
2022-01-05 21:26:31 +0530

    modify file

--- script/index.js
+++ script/index.js
@@ -4,7 +4,7 @@ let searchDiv = document.getElementById("search_result");
             const video_query = document.getElementById("video").value;
 
 
-            let responce = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${video_query}&type=video&key=AIzaSyCgsyGlp11ZNXS9sRV2eBhqEkXZLUqITps&maxResults=20`);
+            let responce = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${video_query}&type=video&key=AIzaSyCgsyGlp11ZNXS9sRV2eBhqEkXZLUqITps&maxResults=20&part=snippet`);
 
             let data = await responce.json();
             console.log("Data : ", data);
@@ -23,18 +23,20 @@ let searchDiv = document.getElementById("search_result");
 
     const appendVideos = (reslts) => {
         searchDiv.innerHTML = null;
-        reslts.map((items) => {
+        reslts.map(({snippet,  id: { videoId } } ) => {
 
-            let {
-                id: { videoId },
-            } = items;
-            console.log("Video Id", videoId);
+            // console.log("snippet", snippet);
+            let div = document.createElement("div");
+            let title= document.createElement("h3");
+            title.innerText = snippet.title;
 
-            let iframe = document.createElement("iframe");
-            iframe.src = `https://www.youtube.com/embed/${videoId}`;
-            iframe.setAttribute("allowfullscreen", "true");
+            let thumbnail = document.createElement("img");
+            thumbnail.src = snippet.thumbnails.medium.url;
 
-            searchDiv.append(iframe);
+            div.append(thumbnail,title);
+            
+
+            searchDiv.append(div);
         });
     }
 
